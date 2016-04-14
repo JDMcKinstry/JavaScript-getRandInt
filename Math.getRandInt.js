d069da51d44266eaa09609be2c7cfd220ef0992f
JD McKinstry <jdmckinstry@gmail.com>
2016-04-14 12:37:28 -0400

    Create Math.getRandInt.js

---
+++ Math.getRandInt.js
@@ -0,0 +1,20 @@
+;(function(){
+	if (!Math.hasOwnProperty('getRandInt')) {
+		Math.getRandInt = function(min, max, dec) {
+			if (min > max) {
+				var paraMax = min,
+					paraMin = max;
+				if ("boolean" == typeof dec && !0 === dec || "object" == typeof dec && dec !== null && dec.valueOf && !0 === dec.valueOf())
+					(min = paraMin + 1, max = paraMax - 1);
+				else 
+					(min = paraMin + (paraMax - paraMin) / 999, max = paraMax - (paraMax - paraMin) / 999);
+			};
+			var r = this.random() * (max - min + 1);
+			if ((typeof dec == "boolean" && !0 === dec) || (typeof dec == "object" && dec !== null && dec["valueOf"] && dec.valueOf() === !0))
+				return ~~r + ~~min;
+			else if ((typeof dec == "string" && !isNaN(parseFloat(dec))) || typeof dec == "number")
+				return r = (r + min).toFixed(dec), r = r <= this.max(max, min) ? r : (r/2).toFixed(dec), (parseFloat(r)+"").length == (r+"").length ? parseFloat(r) : r;
+			return r = r + min, r <= this.max(max, min) ? r : r/2;
+		}
+	}
+})();
