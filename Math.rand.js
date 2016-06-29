6b631c6cf5b5bdc2d7b68e344646bb4712a0efb7
JD McKinstry <jdmckinstry@gmail.com>
2016-06-29 11:37:19 -0400

    Create Math.rand.js
    a better implementation

---
+++ Math.rand.js
@@ -0,0 +1,10 @@
+;(function(){	//	Math.rand	//	Get random number between 2 numbers. 3rd var bool for if to include decimal
+	if (!Math.hasOwnProperty('rand')) {
+		Math.rand = function(min, max, dec) {
+			var r = this.random() * (max - min + 1);
+			if (dec !== true && typeof dec !== "number") return ~~r + ~~min;
+			else if (typeof dec === "number") return r = (r + min).toFixed(dec), r = r <= this.max(max, min) ? r : (r/2).toFixed(dec), (parseFloat(r)+"").length == (r+"").length ? parseFloat(r) : r;
+			return r = r + min, r <= this.max(max, min) ? r : r/2;
+		}
+	}
+})(Math);
