02aee2b5ebee64e11fc8ef3b599da5eb3b688416
JD McKinstry <jdmckinstry@gmail.com>
2017-11-03 10:38:03 -0400

    Update Math.rand.js

--- Math.rand.js
+++ Math.rand.js
@@ -1,6 +1,6 @@
-;(function(){	//	Math.rand	//	Get random number between 2 numbers. 3rd var bool for if to include decimal
-	if (!Math.hasOwnProperty('rand')) {
-		Math.rand = function(min, max, dec) {
+;(function(){	//	Math.getRandInt	//	Get random number between 2 numbers. 3rd var bool for if to include decimal
+	if (!Math.hasOwnProperty('getRandInt')) {
+		Math.getRandInt = function(min, max, dec) {
 			if (min > max && 2 <= min - max) { var temp = min-1; min = max+1; max = temp; }
 			var r = this.random() * (max - min + 1);
 			if (dec !== true && typeof dec !== "number") return ~~r + ~~min;
